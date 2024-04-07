77ef7783c15080bd9f34414d23967dec59db68ab
Chin Trinh <35022967+chnirt@users.noreply.github.com>
2024-04-08 00:00:47 +0700

    feat: navbar version 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,156 +32,126 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav>
-      <div className="fixed top-0 z-10 flex w-full flex-col items-end">
-        <div className="flex w-full justify-center backdrop-blur">
-          <div className="responsive-container container flex flex-row justify-between py-4 md:py-5 lg:py-5">
-            <div className="flex items-center gap-9">
-              <Link href={"/"}>
-                <div className="max-w-32">{StaticImage.logo}</div>
-              </Link>
-              <div className="hidden gap-7 text-sm font-semibold text-neutral-900 lg:flex">
-                {links.map((link, li) => {
-                  const isSelected = link.href === pathname;
-                  return (
-                    <Link
-                      key={["link", li].join("-")}
-                      href={link.href}
-                      className="group"
+    <nav className="fixed top-0 z-10 flex w-full flex-col items-end">
+      <div className="flex w-full justify-center backdrop-blur">
+        <div className="container flex items-center justify-between px-5 py-4 sm:px-5 sm:py-4 md:px-10 md:py-5 lg:px-0 lg:py-5">
+          <div className="flex gap-10">
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                router.push("/");
+                setOpen(false);
+              }}
+            >
+              <div className="max-w-32">{StaticImage.logo}</div>
+            </a>
+
+            <div
+              className={twMerge(
+                "hidden gap-7 text-sm font-semibold text-neutral-900 transition-all lg:flex lg:items-center",
+                open &&
+                  "absolute left-0 top-0 z-10 flex min-h-dvh w-full flex-1 flex-col items-center justify-center bg-black/50 text-4xl text-white",
+              )}
+            >
+              {[
+                ...(open
+                  ? [
+                      {
+                        name: "Home",
+                        href: "/",
+                      },
+                    ]
+                  : []),
+                ...links,
+              ].map((link, li) => {
+                const isSelected = link.href === pathname && !open;
+                return (
+                  <a
+                    key={["link", li].join("-")}
+                    className={twMerge(
+                      "group animate-fade-left",
+                      li === 0 && "animate-delay-0",
+                      li === 1 && "animate-delay-75",
+                      li === 2 && "animate-delay-100",
+                      li === 3 && "animate-delay-200",
+                    )}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      // router.push(link.href);
+                      setOpen(false);
+                    }}
+                  >
+                    <span
+                      className={twMerge(
+                        "transition-all group-hover:text-primary",
+                        isSelected && "text-primary",
+                      )}
                     >
-                      <span
-                        className={twMerge(
-                          "transition-all group-hover:text-primary",
-                          isSelected && "text-primary",
-                        )}
-                      >
-                        {link.name}
-                      </span>
-                      <span
-                        className={twMerge(
-                          "block h-0.5 max-w-0 bg-primary transition-all group-hover:max-w-full",
-                          isSelected && "max-w-full",
-                        )}
-                      />
-                    </Link>
-                  );
-                })}
-              </div>
-            </div>
-
-            <div className="flex flex-row items-center gap-3">
-              <button className="hidden max-w-32 md:flex">
-                {StaticImage.googlePlay}
-              </button>
-              <button className="hidden max-w-32 md:flex">
-                {StaticImage.appStore}
-              </button>
-
-              <button
-                className="flex h-10 w-10 items-center justify-center text-neutral-900 transition-all lg:hidden"
-                onClick={() => setOpen((prevState) => !prevState)}
-              >
-                {open ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18 18 6M6 6l12 12"
+                      {link.name}
+                    </span>
+                    <span
+                      className={twMerge(
+                        "block h-0.5 max-w-0 bg-primary transition-all group-hover:max-w-full",
+                        isSelected && "max-w-full",
+                      )}
                     />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                    />
-                  </svg>
-                )}
-              </button>
+                  </a>
+                );
+              })}
             </div>
           </div>
-        </div>
 
-        <Link
-          href={"https://github.com/chnirt/landify-landing-page"}
-          target="_blank"
-        >
-          <Octocat />
-        </Link>
+          <div className="flex gap-4">
+            <div className="hidden gap-3 md:flex">
+              <button className="max-w-32">{StaticImage.googlePlay}</button>
+              <button className="max-w-32">{StaticImage.appStore}</button>
+            </div>
+            <button
+              className="z-20 h-10 w-10 items-center justify-center text-neutral-900 transition-all lg:hidden"
+              onClick={() => setOpen((prevState) => !prevState)}
+            >
+              {open ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div
-        className={twMerge(
-          "fixed left-1/2 top-1/2 z-10 hidden h-full w-full -translate-x-1/2 -translate-y-1/2 bg-black/50 transition-all",
-          open && "inline lg:hidden",
-        )}
-        onClick={() => {
-          setOpen(false);
-        }}
+      <Link
+        href={"https://github.com/chnirt/landify-landing-page"}
+        target="_blank"
       >
-        <div className="flex h-full flex-col items-center justify-center gap-7 text-xl font-semibold text-neutral-300 backdrop-blur-sm transition-all lg:flex lg:flex-col">
-          <a
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/");
-              setOpen(false);
-            }}
-          >
-            <span>Home</span>
-          </a>
-          <a
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/features");
-              setOpen(false);
-            }}
-          >
-            <span>Features</span>
-          </a>
-          <a
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/pricing");
-              setOpen(false);
-            }}
-          >
-            <span>Pricing</span>
-          </a>
-          <a
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/careers");
-              setOpen(false);
-            }}
-          >
-            <span>Careers</span>
-          </a>
-          <a
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/help");
-              setOpen(false);
-            }}
-          >
-            <span>Help</span>
-          </a>
-        </div>
-      </div>
+        <Octocat />
+      </Link>
     </nav>
   );
 };
