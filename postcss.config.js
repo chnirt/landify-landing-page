1bb1aa31caabe58cb623151bcc3e12013470c9e8
Chin Trinh <35022967+chnirt@users.noreply.github.com>
2024-04-28 17:39:30 +0700

    bug: fix build

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    "autoprefixer",
-    purgecss, // Sử dụng purgecss như một phần tử trong mảng plugins
-  ],
+  plugins: ["tailwindcss", "autoprefixer"],
 };
